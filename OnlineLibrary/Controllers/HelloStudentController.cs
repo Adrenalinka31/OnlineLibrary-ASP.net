bc7799d5cace1e4ffa9c998de3176f1f0a8f791c
Maestro-Rus <gagatik316@gmail.com>
2023-03-28 15:11:51 +0300

    Добавьте файлы проекта.

---
+++ OnlineLibrary/Controllers/HelloStudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineLibrary.Models;
+
+namespace OnlineLibrary.Controllers
+{
+    public class HelloStudentController : Controller
+    {
+        private static List<StudentViewModel> stud = new List<StudentViewModel>();
+        public IActionResult Index()
+        {
+            return View(stud);
+        }
+        public IActionResult Create()
+        {
+            var studVm = new StudentViewModel();
+            return View(studVm);
+        }
+        public IActionResult CreateStudent(StudentViewModel studentViewModel)
+        {
+            
+            stud.Add(studentViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+        public String Hello()
+        {
+            return "Кто здесь?";
+        }
+    }
+}
---
+++ OnlineLibrary/Models/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineLibrary.Models
+{
+    public class StudentViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
