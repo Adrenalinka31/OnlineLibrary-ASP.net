96085a3a5bfe11712c641c109f8444f2b2604877
Maestro-Rus <kirillvolod@gmail.com>
2023-05-02 13:58:40 +0300

    Создал представления для регистрации и входа, добавил контроллер для регистрации

--- OnlineLibrary/Controllers/AccountController.cs
+++ OnlineLibrary/Controllers/AccountController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using OnlineLibrary.Domain.ViewModels.Account;
+using OnlineLibrary.Service.Interfaces;
+using System.Security.Claims;
 
 namespace OnlineLibrary.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IAccountService _accountService;
 
-        public IActionResult Index()
+        public AccountController(IAccountService accountService)
         {
-            return View();
+            _accountService = accountService;
+        }
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _accountService.Register(model);
+                if(response.StatusCode == Domain.Enum.StatusCode.OK)
+                {
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(response.Data));
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", response.Description);
+            }
+            return View(model);
         }
     }
 }
